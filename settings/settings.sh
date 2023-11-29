61a4f3e68a12c1c4ee5744c02d3edd01d4b94cb9
Sam Gamble <hexadessa@gmail.com>
2023-11-29 13:56:02 +0000

    Fix

--- settings/settings.sh
+++ settings/settings.sh
@@ -1,16 +1,16 @@
 # Azure location - Use "Name" from Azure CLI:
 # az account list-locations -o table
-LOCATION="centralus"
+LOCATION="northcentralus"
 
 # DNS name - must be unique within Azure and only contain characters permitted in domain names.
 # Deployment will be accessible at ${DNS_LABEL_NAME}.${LOCATION}.cloudapp.azure.com after deployment.
-DNS_LABEL_NAME="oasis-enterprise-devtester"
+DNS_LABEL_NAME=""
 
 # Email to use for letsencrypt certificates
-LETSENCRYPT_EMAIL="rodney.effah@oasislmf.org"
+LETSENCRYPT_EMAIL=""
 
 # Name of the resource group to deploy to
-RESOURCE_GROUP="oasis-enterprise-devtester"
+RESOURCE_GROUP="oasis-enterprise"
 
 # Image build settings - tell pip to trust certificates from pypi.org and files.pythonhosted.org,
 # do no verify them. In case your want to build locally and are behind a corporate proxy.
@@ -18,10 +18,10 @@ RESOURCE_GROUP="oasis-enterprise-devtester"
 
 # Set to override path to OasisLMF/OasisPlatform repository with either branch platform-2.0-azure-sprint-2 checked
 # out or platform-2.0 (if previous is merged)
-OASIS_PLATFORM_DIR=/home/rodney/Documents/OasisLMF/OasisPlatform
+#OASIS_PLATFORM_DIR=~/git/OasisPlatform/
 
 # Set to override path to OasisLMF/OasisPiWind repository
-OASIS_PIWIND_DIR=/home/rodney/Documents/OasisLMF/OasisPiWind
+#OASIS_PIWIND_DIR=~/git/OasisPiWind/
 
 # Override the standard azure parameter file
 #AZURE_PARAM_FILE="${SCRIPT_DIR}/settings/azure/myparameters.json"
